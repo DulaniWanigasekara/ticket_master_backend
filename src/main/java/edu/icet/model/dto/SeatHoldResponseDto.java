79c54d0c4f4ac11401ef2d0bf335a63210bb108a
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 13:50:10 +0530

    SeatResponseDto class completed

--- src/main/java/edu/icet/model/dto/SeatHoldResponseDto.java
+++ src/main/java/edu/icet/model/dto/SeatHoldResponseDto.java
@@ -1,4 +1,20 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class SeatHoldResponseDto {
+    private Long id;
+    private String seatNumber;
+    private String status;
+    private Long heldByUserId;
+    private LocalDateTime holdExpiry;
+    private long remainingSeconds;
 }
