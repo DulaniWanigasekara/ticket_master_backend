1853697a950a8dc5d1c71c1b4772aa33c1ade771
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 15:12:07 +0530

    ErrorResponceDto completed

--- src/main/java/edu/icet/model/dto/ErrorReponseDto.java
+++ src/main/java/edu/icet/model/dto/ErrorReponseDto.java
@@ -1,4 +1,19 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class ErrorReponseDto {
+    private String errorCode;
+    private String message;
+    private String details;
+    private LocalDateTime timestamp;
+    private Integer remainingSeconds;
 }
