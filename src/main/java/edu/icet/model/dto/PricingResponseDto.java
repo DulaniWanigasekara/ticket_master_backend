6c6bf0d20c90cb378791e88f16ceab40bc4fc39d
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 15:09:11 +0530

    PricingResponsedto class completed

--- src/main/java/edu/icet/model/dto/PricingResponseDto.java
+++ src/main/java/edu/icet/model/dto/PricingResponseDto.java
@@ -1,4 +1,18 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
 public class PricingResponseDto {
+    private Double basePrice;
+    private String userTier;
+    private Double finalPrice;
+    private Integer discountPercentage;
+    private Boolean priorityAccess;
+    private String note;
 }
