51b14b79e31c55270a4672029656e847eca2bf2d
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:29:15 +0530

    SeatsRepository extented

--- src/main/java/edu/icet/repository/SeatsRepository.java
+++ src/main/java/edu/icet/repository/SeatsRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface SeatsRepository {
+import edu.icet.model.entity.Seats;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SeatsRepository extends JpaRepository<Seats, Long> {
 }
