b255e8caa33fc69d70f52a679ca5e493b989f535
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 13:22:02 +0530

    AuditLogs Entity completed

--- src/main/java/edu/icet/model/entity/AuditLogs.java
+++ src/main/java/edu/icet/model/entity/AuditLogs.java
@@ -1,4 +1,29 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "audit_logs")
 public class AuditLogs {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String action;
+
+    private Long userId;
+
+    private String details;
+
+    private LocalDateTime timestamp;
 }
