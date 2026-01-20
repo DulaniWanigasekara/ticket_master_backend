5815f5af032f3317a606c69dff4ee57e013f2388
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:26:46 +0530

    AuditLogsRepository created

--- src/main/java/edu/icet/repository/AuditLogsRepository.java
+++ src/main/java/edu/icet/repository/AuditLogsRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface AuditLogsRepository {
+import edu.icet.model.entity.AuditLogs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogsRepository extends JpaRepository<AuditLogs, Long> {
 }
