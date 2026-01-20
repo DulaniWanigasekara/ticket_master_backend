ae6280b814e8014f3c9ac0e5a70bcbd1b640db86
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 15:18:31 +0530

    BookingRepository Updated

--- src/main/java/edu/icet/repository/BookingsRepository.java
+++ src/main/java/edu/icet/repository/BookingsRepository.java
@@ -4,6 +4,13 @@ import edu.icet.model.entity.Bookings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BookingsRepository extends JpaRepository<Bookings, Long> {
+
+    List<Bookings> findByUsers_Id(Long userId);
+
+    Optional<Bookings> findBySeats_Id(Long seatId);
 }
