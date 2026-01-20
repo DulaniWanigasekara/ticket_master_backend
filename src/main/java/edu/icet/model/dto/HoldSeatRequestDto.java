fc5036549b1dd9cc3e6a7210f01eaa3d4a889886
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 15:05:19 +0530

    HoldSeatRequestDto completed

--- src/main/java/edu/icet/model/dto/HoldSeatRequestDto.java
+++ src/main/java/edu/icet/model/dto/HoldSeatRequestDto.java
@@ -1,4 +1,13 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class HoldSeatRequestDto {
+    private Long userId;
+    private Long seatId;
 }
