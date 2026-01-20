0cd20f320182c42deeeddd08bb38283222d55ac3
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:21:29 +0530

    EventsRepository Completed

--- src/main/java/edu/icet/repository/EventsRepository.java
+++ src/main/java/edu/icet/repository/EventsRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface EventsRepository {
+import edu.icet.model.entity.Events;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventsRepository extends JpaRepository<Events, Long> {
 }
