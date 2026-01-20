13369ad64e3a69f566416618d50c22cee1ae75aa
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:06:28 +0530

    BookingResponseDto class completed

--- src/main/java/edu/icet/model/dto/BookingRequestDto.java
+++ src/main/java/edu/icet/model/dto/BookingRequestDto.java
@@ -1,4 +1,12 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class BookingRequestDto {
+    private String seatId;
 }
--- src/main/java/edu/icet/model/dto/BookingResponseDto.java
+++ src/main/java/edu/icet/model/dto/BookingResponseDto.java
@@ -1,4 +1,20 @@
 package edu.icet.model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class BookingResponseDto {
+        private Long bookingId;
+        private Long userId;
+        private Long seatId;
+        private String seatNumber;
+        private double amountPaid;
+        private String status;
+        private boolean priorityAccess;
+
+
 }
