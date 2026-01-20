a0fbabe847b790964cfead02db45686005f0265f
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 13:18:18 +0530

    Booking Entity completed

--- src/main/java/edu/icet/model/entity/Bookings.java
+++ src/main/java/edu/icet/model/entity/Bookings.java
@@ -1,4 +1,31 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "bookings")
 public class Bookings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @OneToOne
+    @JoinColumn(name = "seat_id")
+    private Seats seats;
+
+    private double amountPaid;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
 }
