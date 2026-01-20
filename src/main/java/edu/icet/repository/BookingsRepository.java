85b535aec044294e86010c56a50894601ac16abc
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:23:36 +0530

    BookingRepository completed

--- src/main/java/edu/icet/repository/BookingsRepository.java
+++ src/main/java/edu/icet/repository/BookingsRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface BookingsRepository {
+import edu.icet.model.entity.Bookings;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingsRepository extends JpaRepository<Bookings, Long> {
 }
