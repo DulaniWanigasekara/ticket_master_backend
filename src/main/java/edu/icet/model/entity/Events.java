91f0ee3e3f5fc1d450fa2f1055ab0549766530b6
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 13:11:30 +0530

    BookingStatus, UserTier, SeatStatus Enums and Users, Events, Seats Entities completed

--- src/main/java/edu/icet/model/entity/Events.java
+++ src/main/java/edu/icet/model/entity/Events.java
@@ -1,4 +1,25 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table( name = "events")
 public class Events {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double basePrice;
+    private boolean idHighDemand;
+    private LocalDateTime eventDate;
 }
--- src/main/java/edu/icet/model/entity/Seats.java
+++ src/main/java/edu/icet/model/entity/Seats.java
@@ -1,5 +1,41 @@
 package edu.icet.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "Seats")
 public class Seats {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String seatNumber;
+
+    @Enumerated(EnumType.STRING)
+    private SeatStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Events events;
+
+    @ManyToOne
+    @JoinColumn(name = "held_by_user_id")
+    private Users heldBy;
+
+    private LocalDateTime holdExpiry;
+
+    @Version
+    private Long version;
+
+
 
 }
