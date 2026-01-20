3d21403d279d00daf0f62dc919ead7fddf39fc07
Dulani Wanigasekara <dulani.piusha2003@gmail.com>
2026-01-20 14:20:02 +0530

    UsersRepository completed

--- src/main/java/edu/icet/repository/UsersRepository.java
+++ src/main/java/edu/icet/repository/UsersRepository.java
@@ -1,4 +1,9 @@
 package edu.icet.repository;
 
-public interface UsersRepository {
+import edu.icet.model.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsersRepository extends JpaRepository <Users, Long> {
 }
